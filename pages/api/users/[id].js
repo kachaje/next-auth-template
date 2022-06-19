b6d25f13cdf02d309afdc14b00d9ca145a0e9197
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2022-06-19 16:17:11 +0200

    Updated api code

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,30 +1,8 @@
-const User = require("../../../lib/users/user");
-const Users = require("../../../lib/users/users");
-
-const handleGet = async (req, res) => {
-  console.log(req.query);
-  const id = parseInt(req.query.id, 10);
-
-  try {
-    const user = await Users.find(id);
-
-    if (user) {
-      return res.status(200).send(user.toJson());
-    }
-
-    res.status(404).send("item not found");
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
-};
-
-const handlePut = async (req, res) => {
-  res.status(200).json({ msg: "Welcome PUT" });
-};
-
-const handleDelete = async (req, res) => {
-  res.status(200).json({ msg: "Welcome DELETE" });
-};
+const {
+  handleGet,
+  handlePut,
+  handleDelete,
+} = require("../../../lib/users/services");
 
 export default async function handler(req, res) {
   console.log(req.method);
